22e115351ae7007bdd01cc01fb678da40577347f
Raveryk <raveryk@gmail.com>
2021-11-19 13:52:09 -0600

    first commit.

---
+++ DutchTreat/Startup.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace DutchTreat
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllersWithViews();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // the order of these methods is important
+
+            // this middleware looks for files that it could serve and changes the path
+            //app.UseDefaultFiles();
+
+            // tells the application to be able to serve static files
+            app.UseStaticFiles();
+            // allows us to route individual calls to specific pieces of code
+            app.UseRouting();
+
+            app.UseEndpoints(cfg =>
+            {
+                cfg.MapControllerRoute("Default",
+                    "/{controller}/{action}/{id?}",
+                new { controller = "App", action = "Index"});
+            });
+        }
+    }
+}
---
+++ DutchTreat/wwwroot/js/index.js
@@ -0,0 +1,22 @@
+﻿$(document).ready(function () {
+
+    let theForm = $('#theForm');
+    theForm.hide();
+
+    let button = $('#buyButton');
+    button.on("click", function () {
+        console.log("Buying Item")
+    });
+
+    let productInfo = $(".product-info li");
+    productInfo.on('click', function () {
+        console.log("You clicked on " + $(this).text());
+    });
+
+    const $loginToggle = $('#loginToggle');
+    const $popupForm = $('.popup-form');
+
+    $loginToggle.on('click', function () {
+        $popupForm.fadeToggle(100);
+    })
+});
